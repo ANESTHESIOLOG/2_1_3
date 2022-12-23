cc1449ae12ae8c11e30c387593cda92da53c2594
zotov <zotovegor952@gmail.com>
2022-12-23 21:18:33 +0300

    Решение

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("catBean");
+        System.out.println(beanCat1.getMessage());
+
+        Cat beanCat2 = (Cat) applicationContext.getBean("catBean");
+        System.out.println(beanCat2.getMessage());
+
+        System.out.println("Bean singleton: " + (bean == bean2));
+        System.out.println("Bean prototype: " + (beanCat1 == beanCat2));
     }
 }
